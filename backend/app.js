067626b67e1cc0519ffcf1f3af2c3d321aee8628
SenthurselvanSatheeskumar <satheeskumar9998@gmail.com>
2025-09-18 23:17:52 +0530

    add route to deployment

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,10 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.get('/', async (req, res) => {
+  res.json("backend is running");
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/product', productRoutes);
 app.use('/api/cart', cartRoutes);
